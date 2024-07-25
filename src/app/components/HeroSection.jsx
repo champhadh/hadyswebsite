602a3a24da98b3c4d4dadbae87114ccd3045c1db
Hady Wehbe <110308285+champhadh@users.noreply.github.com>
2024-07-25 15:32:47 -0400

    commit message

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -67,8 +67,8 @@ const HeroSection = () => {
             <Image
               src="/images/hero-image.png"
               className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-              width={2}
-              height={150}
+              width={220}
+              height={220}
             />
           </div>
         </motion.div>
